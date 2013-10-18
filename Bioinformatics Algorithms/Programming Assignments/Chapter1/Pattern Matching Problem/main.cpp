8e73d3fbf42cb66df24b0b088fc4d6717cb8259e
yangzhang <v-yza@v-yza.(none)>
2013-10-18 10:22:41 +0800

    add pattern

---
+++ Bioinformatics Algorithms/Programming Assignments/Chapter1/Pattern Matching Problem/main.cpp
@@ -0,0 +1,70 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+vector<int> find_substring(string pattern, string text) {
+	int n = pattern.size();
+	vector<int> next(n + 1, 0);
+	for (int i = 1; i < n; i++) {
+		int j = i;
+		while (j > 0) {
+			j = next[j];
+			if (pattern[j] == pattern[i]) {
+				next[i + 1] = j + 1;
+				break;
+			}
+		}
+	}
+
+	vector<int> position;
+	int m = text.size();
+	for (int i = 0, j = 0; i < m; i++) {
+		if (j < n && text[i] == pattern[j]) {
+			j++;
+		} else {
+			while (j > 0) {
+				j = next[j];
+				if (text[i] == pattern[j]) {
+					j++;
+					break;
+				}
+			}
+		}
+		if (j == n) {
+			position.push_back(i - n + 1);
+		} 
+	}
+	return position;
+}
+
+int main() {
+	string pattern, text;
+	//cin >> pattern >> text;
+	pattern = "CTTGATCAT";
+	cin >> text;
+	vector<int> ans = find_substring(pattern, text);
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
